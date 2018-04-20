bd5b1f356184dd93b205ed4eaafc5e529f3613ce
Limiteddd <38568217+Limiteddd@users.noreply.github.com>
2018-04-20 20:42:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,15 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-switch (command) {
-  case "ping" :
-    message.channel.send('Pong!');
-    break;
-  case "blah" :
-    message.channel.send('Meh.');
-    break;
-}
- 
+if (command === "asl") {
+  let age = args[0]; // Remember arrays are 0-based!.
+  let sex = args[1];
+  let location = args[2];
+  message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
 
 });
 
