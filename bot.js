50b26bca8a9172ed99c709c8fc5f15bfce9d1d93
Limiteddd <38568217+Limiteddd@users.noreply.github.com>
2018-04-20 20:31:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'torrent') {
+    	message.reply('Úristen kalózkodós poor kid vagy,vedd meg inkább a játékot ne töltsd le torrentel!');
   	}
 });
 
