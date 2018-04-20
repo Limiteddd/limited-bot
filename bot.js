218a59eed67d2c3e3921c4a236e7776d1946dffb
Limiteddd <38568217+Limiteddd@users.noreply.github.com>
2018-04-20 20:46:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-if (command === "asl") {
-  let age = args[0]; // Remember arrays are 0-based!.
-  let sex = args[1];
-  let location = args[2];
-  message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
-
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
++const Discord = require('discord.js');
++const client = new Discord.Client();
++
++client.on('ready', () => {
++    console.log('I am ready!');
++});
++
++client.on('message', message => {
++    if (message.content === 'csicska') {
++    	message.reply('halj meg');
++  	}
++});
++
++// THIS  MUST  BE  THIS  WAY
++client.login(process.env.BOT_TOKEN);
